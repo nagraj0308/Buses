889a6bd737b9d6214cb5d312dfc0dee5d2ed3b30
Nagendra Chaudhary <nagendrachaudhary2016@gmail.com>
2019-09-26 17:32:31 +0530

    simple commit

---
+++ app/src/main/java/com/nagraj/buses/MainActivity.java
@@ -0,0 +1,17 @@
+package com.nagraj.buses;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Bengaluru to Chennai");
+
+
+    }
+}
